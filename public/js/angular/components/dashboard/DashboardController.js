619b4c6cc487e324210910b29e184dc2d9ba035e
Omar Furrer <omar.furrer@gmail.com>
2017-05-09 23:52:14 +0200

    Fix pagonation error

--- public/js/angular/components/dashboard/DashboardController.js
+++ public/js/angular/components/dashboard/DashboardController.js
@@ -537,6 +537,14 @@
             return null;
         }
 
+        vm.isEmpty = function (obj) {
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop))
+                    return false;
+            }
+            return true;
+        };
+
 //        vm.index();
         vm.indexFolders();
 
